58a4cbc3a3ad9fcbf09750be0c1d91d0fdc2a338
sunidhi7777 <sunidhi777777@gmail.com>
2026-08-08 00:50:12 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool check(int i , int j , int m , int n){
+        if(i<0 || i>=n || j<0 || j>=m){
+            return false;
+        }
+        return true ;
+    }
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int INF = INT_MAX; 
+        int n = grid.size() ;
+        int m = grid[0].size() ;
+        queue<pair<int,int>>  q ;
+        for(int i=0 ; i<n ;i++){
+            for(int j=0 ; j<m;j++){
+                if(grid[i][j]==0)
+                q.push({i,j});
+            }
+        }
+        int distance=1 ;
+        while(!q.empty()){
+            int size=q.size();
+            while(size>0){
+                auto it = q.front();
+                q.pop();
+                int i = it.first;
+                int j = it.second ;
+                vector<int> row = {-1,1,0,0};
+                vector<int> col = {0,0,-1,1};
+
+                for(int k=0;k<4;k++){
+                    int r = i+row[k];
+                    int c = j+col[k];
+
+                    if (!check(r, c, m, n))
+                      continue;
+                    if(grid[r][c]==0)
+                      continue ;
+                    if(check(r,c,m,n) && grid[r][c]==INF){
+                        grid[r][c]=distance ;
+                        q.push({r,c});
+                    }
+                }
+                size -- ;
+            }
+            distance ++ ;
+        }
+    }
+};
